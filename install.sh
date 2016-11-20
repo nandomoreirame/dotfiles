c4a8a36d936a9042bb32d45cf6f6388b5ba12be3
Fernando Moreira <nandomoreira@MacBookPro.local>
2016-11-20 04:02:41 -0200

    Install Homebrew for package management, if not installed

--- install.sh
+++ install.sh
@@ -2,3 +2,17 @@
 
 # Ask for the administrator password upfront
 sudo -v
+
+# --------------–-------–--------–--------–-------–--–-----
+# System packages
+# --------------–-------–--------–--------–-------–--–-----
+# Install Homebrew for package management, if not installed
+if test ! $(which brew)
+then
+  echo " → Installing Homebrew for package management..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure Homebrew is up to date
+brew update
+brew upgrade
