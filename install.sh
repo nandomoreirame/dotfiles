2a769168faa6d42272abf56431b13be125cc0958
Fernando Moreira <nandomoreira.me@gmail.com>
2016-11-20 04:08:25 -0200

    Add function for debuggin

--- install.sh
+++ install.sh
@@ -3,16 +3,24 @@
 # Ask for the administrator password upfront
 sudo -v
 
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+debugging () {
+  echo "${green} → $1${reset}"
+}
+
 # --------------–-------–--------–--------–-------–--–-----
 # System packages
 # --------------–-------–--------–--------–-------–--–-----
 # Install Homebrew for package management, if not installed
 if test ! $(which brew)
 then
-  echo " → Installing Homebrew for package management..."
+  debugging "Installing Homebrew for package management..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+debugging "update and upgrade Homebrew..."
 # Make sure Homebrew is up to date
 brew update
 brew upgrade
