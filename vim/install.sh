b5de4fba60a66abc9c4e32a69e8aae0040397b5d
Fernando Moreira <nandomoreira.me@gmail.com>
2016-12-12 13:19:00 -0200

    vim plugins

--- vim/install.sh
+++ vim/install.sh
@@ -8,5 +8,5 @@ debugging "Linking VIM dotfiles..."
 rm -rf $HOME/.vim $HOME/.gitattributes $HOME/.vim
 rm -rf $HOME/.vimrc $HOME/.gitattributes $HOME/.vimrc
 
-ln -s $HOME/.files/vim/plugins $HOME/.vim
+ln -s $HOME/.files/vim $HOME/.vim
 ln -s $HOME/.files/vim/vimrc $HOME/.vimrc
