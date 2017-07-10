daf81bb9d0b913e6f569a902887e62e43a69cd6d
Fernando Moreira <nandomoreira.me@gmail.com>
2017-07-10 14:07:27 -0300

    Install Sublime text 3

--- sublime/install.sh
+++ sublime/install.sh
@@ -4,19 +4,20 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
     && . "../functions.sh"
 
 if [ "$(uname)" == "Darwin" ]; then
-  debugging "Linking Sublime Text 3 settings on MacOS X..."
-
+  debugging "Linking Sublime Text 3 settings on MacOS..."
   rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
   ln -s ~/.files/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-
   rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
   ln -s ~/.files/sublime/osx/Preferences ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  debugging "Linking Sublime Text 3 settings on Linux..."
+  debugging "Instaling Sublime Text 3 on Linux..."
+  sudo add-apt-repository ppa:webupd8team/sublime-text-3 -qqy
+  sudo apt-get update -qqy
+  sudo apt-get install sublime-text-installer -qqy
 
+  debugging "Linking Sublime Text 3 settings on Linux..."
   rm -rf ~/.config/sublime-text-3/Packages/User
   ln -s ~/.files/sublime/User ~/.config/sublime-text-3/Packages/User
-
   rm -rf ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
   ln -s ~/.files/sublime/linux/Preferences ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 fi
