c149d3e4a6f8270d8636c789b49ce90d409b5cf8
Fernando Moreira <nandomoreira.me@gmail.com>
2017-07-20 22:02:12 -0300

    npm init using .npmrc file

--- node/install.sh
+++ node/install.sh
@@ -28,3 +28,18 @@ then
   npm install -g yarn
   yarn global add $(cat globals|grep -v "#")
 fi
+
+rm -rf $HOME/.npmrc
+
+echo -n "${color}npm init name: ${reset}"
+read NPM_INIT_NAME
+echo -n "${color}npm init email: ${reset}"
+read NPM_INIT_EMAIL
+echo -n "${color}npm init utl: ${reset}"
+read NPM_INIT_URL
+
+echo "init.author.name=$NPM_INIT_NAME
+init.author.email=$NPM_INIT_EMAIL
+init.author.url=$NPM_INIT_URL
+init.version=0.0.1
+init.license=MIT" > $HOME/.npmrc
