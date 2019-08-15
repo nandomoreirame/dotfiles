7a6d1b6e3e1821d6979bfbc46ad03846d1d1c42a
Fernando Moreira <nandomoreira.me@gmail.com>
2019-08-15 13:05:41 -0300

    is vscode insiders

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,10 +2,24 @@
 
 source "$HOME/dotfiles/common/functions.sh"
 
-rm -rf $HOME/Library/Application\ Support/Code/User/*.json $HOME/Library/Application\ Support/Code/User/snippets
-ln -s $HOME/dotfiles/vscode/osx/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-ln -s $HOME/dotfiles/vscode/osx/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
-ln -s $HOME/dotfiles/vscode/snippets $HOME/Library/Application\ Support/Code/User/snippets
-ln -s $HOME/dotfiles/vscode/vsicons.settings.json $HOME/Library/Application\ Support/Code/User/vsicons.settings.json
+# is Insiders ?
+echo "is Visual Studio Code - Insiders? (y or n)"
+read isInsiders;
 
-msg_ok "VSCode settings..."
+if [ "$isInsiders" = "y" ];
+then
+  rm -rf $HOME/Library/Application\ Support/Code\ -\ Insiders/User/*.json $HOME/Library/Application\ Support/Code\ -\ Insiders/User/snippets
+  ln -s $HOME/dotfiles/vscode/osx/settings.json $HOME/Library/Application\ Support/Code\ -\ Insiders/User/settings.json
+  ln -s $HOME/dotfiles/vscode/osx/keybindings.json $HOME/Library/Application\ Support/Code\ -\ Insiders/User/keybindings.json
+  ln -s $HOME/dotfiles/vscode/snippets $HOME/Library/Application\ Support/Code\ -\ Insiders/User/snippets
+  ln -s $HOME/dotfiles/vscode/vsicons.settings.json $HOME/Library/Application\ Support/Code\ -\ Insiders/User/vsicons.settings.json
+  msg_ok "Visual Studio Code - Insiders settings..."
+else
+  rm -rf $HOME/Library/Application\ Support/Code/User/*.json $HOME/Library/Application\ Support/Code/User/snippets
+  ln -s $HOME/dotfiles/vscode/osx/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+  ln -s $HOME/dotfiles/vscode/osx/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+  ln -s $HOME/dotfiles/vscode/snippets $HOME/Library/Application\ Support/Code/User/snippets
+  ln -s $HOME/dotfiles/vscode/vsicons.settings.json $HOME/Library/Application\ Support/Code/User/vsicons.settings.json
+  msg_ok "Visual Studio Code settings..."
+fi
+
