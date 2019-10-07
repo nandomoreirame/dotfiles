979a2cd214d20b2483073cb063d015f73bcae948
Fernando Moreira <nandomoreira.me@gmail.com>
2019-10-07 10:54:13 -0300

    .npmrc

--- node/npmrc.sh
+++ node/npmrc.sh
@@ -2,7 +2,9 @@
 
 source "$HOME/dotfiles/common/functions.sh"
 
-NPMRC_FILE="$HOME/.npmrc"
+NPMRC_FILE="$HOME/dotfiles/node/.npmrc"
+
+rm -rf $NPMRC_FILE
 
 NPM_INIT_NAME="Fernando Moreira"
 NPM_INIT_EMAIL="nandomoreira.me@gmail.com"
@@ -14,7 +16,9 @@ echo "init.author.name=$NPM_INIT_NAME
 init.author.email=$NPM_INIT_EMAIL
 init.author.url=$NPM_INIT_URL
 init.version=$NPM_INIT_VERSION
-init.license=$NPM_INIT_LICENSE" > $HOME/.npmrc
+init.license=$NPM_INIT_LICENSE" > $NPMRC_FILE
+
+ln -s $NVMRC_FILE $HOME/.npmrc
 
 msg_checking "successfully created file $NPMRC_FILE"
 
