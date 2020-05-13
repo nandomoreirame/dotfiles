530889c307b973facfae153b61a51746ed66c7e3
Fernando Moreira <nandomoreira.me@gmail.com>
2020-05-13 14:55:51 -0300

    vscode settings

--- vscode/install.sh
+++ vscode/install.sh
@@ -10,3 +10,11 @@ ln -s $DOTFILES/vscode/snippets $HOME/Library/Application\ Support/Code/User/sni
 ln -s $DOTFILES/vscode/vsicons.settings.json $HOME/Library/Application\ Support/Code/User/vsicons.settings.json
 rm -rf $HOME/.vscode/extensions.json && ln -s $DOTFILES/vscode/extensions.json $HOME/.vscode/extensions.json
 bot ""
+
+rm -rf $HOME/Library/Application\ Support/Code\ -\ Insiders/User/*.json $HOME/Library/Application\ Support/Code\ -\ Insiders/User/snippets
+ln -s $DOTFILES/vscode/osx/settings.json $HOME/Library/Application\ Support/Code\ -\ Insiders/User/settings.json
+ln -s $DOTFILES/vscode/osx/keybindings.json $HOME/Library/Application\ Support/Code\ -\ Insiders/User/keybindings.json
+ln -s $DOTFILES/vscode/snippets $HOME/Library/Application\ Support/Code\ -\ Insiders/User/snippets
+ln -s $DOTFILES/vscode/vsicons.settings.json $HOME/Library/Application\ Support/Code\ -\ Insiders/User/vsicons.settings.json
+rm -rf $HOME/.vscode/extensions.json && ln -s $DOTFILES/vscode/extensions.json $HOME/.vscode/extensions.json
+bot ""
